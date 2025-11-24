65e41fd4298fddac01919b5f76079f76ca967718
roberto <roberto.bani@gmail.com>
2025-11-24 19:01:51 +0100

    first commit

---
+++ src/main/java/it/estar/time_counter/TimeCounterApplication.java
@@ -0,0 +1,13 @@
+package it.estar.time_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/estar/time_counter/TimeCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.estar.time_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
