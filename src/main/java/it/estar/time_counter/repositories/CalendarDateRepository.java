eadeacab617d59011ba662ddd5b563527cb4f88c
roberto <roberto.bani@gmail.com>
2025-11-25 15:40:05 +0100

    created repositories

--- src/main/java/it/estar/time_counter/repositories/CalendarDateRepository.java
+++ src/main/java/it/estar/time_counter/repositories/CalendarDateRepository.java
@@ -2,6 +2,8 @@ package it.estar.time_counter.repositories;
 
 import it.estar.time_counter.domain.CalendarDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CalendarDateRepository extends JpaRepository<CalendarDate, Long> {}
 
--- src/main/java/it/estar/time_counter/repositories/CalendarTimeRepository.java
+++ src/main/java/it/estar/time_counter/repositories/CalendarTimeRepository.java
@@ -2,6 +2,8 @@ package it.estar.time_counter.repositories;
 
 import it.estar.time_counter.domain.CalendarTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CalendarTimeRepository extends JpaRepository<CalendarTime, Long> {}
 
